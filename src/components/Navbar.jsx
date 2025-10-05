6c1e559f286ca3aedca247e399a33ff9168093c5
danielmesa801-jpg <danielmesa801@gmail.com>
2025-10-04 22:56:22 -0500

    Correcciones y funcionalidad completa del CRUD

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -3,10 +3,10 @@ import { Link } from 'react-router-dom';
 
 export default function Navbar() {
   return (
-    <nav className="bg-gray-800 p-4 text-white flex space-x-4">
-      <Link to="/" className="hover:underline">Home</Link>
-      <Link to="/users" className="hover:underline">Usuarios</Link>
-      <Link to="/create" className="hover:underline">Crear Usuario</Link>
+    <nav style={{ padding: '1rem', background: '#ddd' }}>
+      <Link to="/" style={{ marginRight: 10 }}>Home</Link>
+      <Link to="/users" style={{ marginRight: 10 }}>Users</Link>
+      <Link to="/create-user" style={{ marginRight: 10 }}>Create User</Link>
     </nav>
   );
 }
